5886aeb781198e12cd33556c4791a913ac7f217a
serpient <littlegangwolf@gmail.com>
2018-05-15 19:17:06 -0700

    update to styling

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   templateUrl: './input-button-unit.component.html',
-  styleUrls: ['./input-button-unit.component.css']
+  styleUrls: ['./input-button-unit.component.css', '../app.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
   @Output() submit: EventEmitter<string> = new EventEmitter();
--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -3,7 +3,7 @@ import { Output, Component, OnInit, Input, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css', '../app.component.css']
 })
 export class TodoItemComponent implements OnInit {
   @Input() item: TodoItem;
